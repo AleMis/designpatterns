e00ef0b97d2b0e36a53662a5c64a81a9670bad2f
AleMis <aleksander.miszkiewicz@gmail.com>
2017-12-16 21:50:14 +0100

    Singleton added

---
+++ src/main/java/com/designpatterns/patterns/singleton/SettingsFileEngine.java
@@ -0,0 +1,43 @@
+package com.designpatterns.patterns.singleton;
+
+public class SettingsFileEngine {
+
+    private static SettingsFileEngine settingsFileEngine = null;
+
+    public static SettingsFileEngine getInstance() {
+        if(settingsFileEngine == null) {
+            settingsFileEngine = new SettingsFileEngine();
+        }
+        return settingsFileEngine;
+    }
+
+    private SettingsFileEngine() {
+    }
+
+
+    private String fileName = "";
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void open(final String fileName) {
+        this.fileName = fileName;
+        System.out.println("Opening the settings file");
+    }
+
+    public void close() {
+        this.fileName = "";
+        System.out.println("Closing the settings file");
+    }
+
+    public boolean loadSettings() {
+        System.out.println("Loading settings from file");
+        return true;
+    }
+
+    public boolean saveSettings() {
+        System.out.println("Saving settings to file");
+        return true;
+    }
+}
